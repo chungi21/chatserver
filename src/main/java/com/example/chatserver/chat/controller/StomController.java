e666041e43a2d5ad606bbea68f04606a78727f67
chungi21 <wlsgml0156@gmail.com>
2025-11-15 02:56:15 +0900

    stomp pub/sub 구조 변경

--- src/main/java/com/example/chatserver/chat/controller/StomController.java
+++ src/main/java/com/example/chatserver/chat/controller/StomController.java
@@ -2,6 +2,9 @@ package com.example.chatserver.chat.controller;
 
 import com.example.chatserver.chat.dto.ChatMessageDto;
 import com.example.chatserver.chat.service.ChatService;
+import com.example.chatserver.chat.service.RedisPubSubService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -13,10 +16,12 @@ public class StomController {
 
     private final SimpMessageSendingOperations messageTemplate;
     private final ChatService chatService;
+    private final RedisPubSubService redisPubSubService;
 
-    public StomController(SimpMessageSendingOperations messageTemplate, ChatService chatService) {
+    public StomController(SimpMessageSendingOperations messageTemplate, ChatService chatService, RedisPubSubService redisPubSubService) {
         this.messageTemplate = messageTemplate;
         this.chatService = chatService;
+        this.redisPubSubService = redisPubSubService;
     }
 
     // 방법1. MessageMapping(수신)과 sendTo(topic에 메시지 전달) 한꺼번에 처리
@@ -31,11 +36,14 @@ public class StomController {
     // why? redis pub/sub 연동시 유연성이 떨어지기 때문에
     @MessageMapping("/{roomId}") // 클라이언트에서 특정 publish/roomId 형태로 메세지 발생시 MessageMapping 수신
     @SendTo("/topic/{roomId}") // 해당 roomId에 메세지를 발행하여 구독 중인 클라이언트에게 메세지 전송
-    public void sendMessage(@DestinationVariable Long roomId, ChatMessageDto chatMessageReqDto) { // @DestinationVariable은 @MessageMapping어노테이션으로 정의된 Websocket Controller 내에서만 사용
-//        System.out.println(chatMessageReqDto.getMessage());
-//        System.out.println(chatMessageReqDto.getSenderEmail());
+    public void sendMessage(@DestinationVariable Long roomId, ChatMessageDto chatMessageReqDto) throws JsonProcessingException { // @DestinationVariable은 @MessageMapping어노테이션으로 정의된 Websocket Controller 내에서만 사용
         chatService.saveMessage(roomId, chatMessageReqDto);
-        messageTemplate.convertAndSend("/topic/"+roomId, chatMessageReqDto);
+        chatMessageReqDto.setRoomId(roomId);
+//        messageTemplate.convertAndSend("/topic/"+roomId, chatMessageReqDto);
+
+        ObjectMapper om = new ObjectMapper();
+        String message = om.writeValueAsString(chatMessageReqDto);
+        redisPubSubService.pulish("chat", message);
     }
 
 }
--- src/main/java/com/example/chatserver/chat/service/RedisPubSubService.java
+++ src/main/java/com/example/chatserver/chat/service/RedisPubSubService.java
@@ -1,5 +1,41 @@
 package com.example.chatserver.chat.service;
 
+import com.example.chatserver.chat.dto.ChatMessageDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
 
-public class RedisPubSubService {
+@Service
+public class RedisPubSubService implements MessageListener {
+
+    private final StringRedisTemplate stringRedisTemplate;
+    private final SimpMessageSendingOperations messageTemplate;
+
+    public RedisPubSubService(@Qualifier("chatPubSub") StringRedisTemplate stringRedisTemplate, SimpMessageSendingOperations messageTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.messageTemplate = messageTemplate;
+    }
+
+    public void pulish(String channel, String message) {
+        stringRedisTemplate.convertAndSend(channel,message);
+    }
+
+    // pattern에는 topic의 이름의 패턴이 담겨져있고, 이 패턴을 기반으로 다이나믹한 코딩 가능
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String payload = new String(message.getBody());
+        ObjectMapper om = new ObjectMapper();
+        try {
+            ChatMessageDto chatMessageDto = om.readValue(payload, ChatMessageDto.class);
+            messageTemplate.convertAndSend("/topic/" + chatMessageDto.getRoomId(), chatMessageDto);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
